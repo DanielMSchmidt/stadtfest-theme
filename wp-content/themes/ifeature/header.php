c50b066e798df6446e6e9999f8061316a6c49d59
Daniel Schmidt <dschmidt@weluse.de>
2013-02-19 23:28:06 +0100

    hided unused stuff

--- wp-content/themes/ifeature/header.php
+++ wp-content/themes/ifeature/header.php
@@ -24,32 +24,32 @@
 <html class="ie ie8 lte9 lte8" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 9]>
-<html class="ie ie9" <?php language_attributes(); ?>> 
+<html class="ie ie9" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if gt IE 9]>  <html <?php language_attributes(); ?>> <![endif]-->
-<!--[if !IE]><!--> 
+<!--[if !IE]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
-	
+
 	<title><?php wp_title(''); ?></title>
-	
+
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	
+
 	<!-- IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/inc/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
-	
+
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php // top header bar section ?>
-		<div class="container-full">
+		<div id="header" class="container-full">
     	<div class="container">
       	<div class="row-fluid">
       	<div class="span6">
@@ -65,13 +65,13 @@
     </div>
 
 <div class="container">
-	
+
 <?php do_action('cyberchimps_before_wrapper'); ?>
 
-<div id="wrapper" class="container-fluid">	
-	
+<div id="wrapper" class="container-fluid">
+
 	<?php do_action('cyberchimps_header'); ?>
-	
+
 	<?php do_action('cyberchimps_before_navigation'); ?>
 
 	<nav id="navigation" role="navigation">
@@ -83,17 +83,17 @@
   					<div class="nav-collapse collapse">
             <?php endif; ?>
           		<?php wp_nav_menu( array( 'theme_location'  => 'primary', 'menu_class' => 'nav', 'walker' => new cyberchimps_walker(), 'fallback_cb' => 'cyberchimps_fallback_menu' ) ); ?>
-			
+
 			<?php if( cyberchimps_get_option( 'searchbar', 1 ) == "1" ) : ?>
-				
+
 					<?php get_search_form(); ?>
-				
+
 			<?php endif; ?>
-      
+
       <?php /* hide collapsing menu if not responsive */
 			if( cyberchimps_get_option( 'responsive_design', 'checked' ) ): ?>
 			</div><!-- collapse -->
-			
+
             <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
               <span class="icon-bar"></span>
@@ -105,5 +105,5 @@
         </div><!-- .navbar-inner .row-fluid -->
       </div><!-- main-navigation navbar -->
 	</nav><!-- #navigation -->
-	
+
 	<?php do_action('cyberchimps_after_navigation'); ?>
